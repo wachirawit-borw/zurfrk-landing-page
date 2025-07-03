12b6de9b2428fce37d8509a1af821f79099c7a83
wachirawit-borw <wachirawit.borw@gmail.com>
2025-07-03 23:27:11 +0700

    อับเดต path เพื่อทำเว็บแนะนำตัวเอง

--- landing-page-project/zurfrk-shoes-landing-page/src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import type { AppProps } from 'next/app';
-import "@/styles/globals.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
